57d33cb54026e66da828cece4583fef3ad687c5c
doroh228 <doroh.2001@gmail.com>
2021-09-11 13:44:04 +0300

    Initial commit

---
+++ app/src/main/java/by/dsn/myapplication1/MainActivity.java
@@ -0,0 +1,14 @@
+package by.dsn.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //TODO: create new world;
+        setContentView(R.layout.activity_main);
+    }
+}
